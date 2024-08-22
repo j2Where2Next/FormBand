2c600cfc6f290d7988b8617abcd2cf6e6744587e
j2.jeremias <j2.jeremias@gmail.com>
2024-08-22 10:07:41 +1000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,25 @@
+members = [
+    {
+        fname: 'John',
+        lname: 'Lennon',
+        instrument: 'Acoustic Guitar'
+    },
+    {
+        fname: 'George',
+        lname: 'Harrison',
+        instrument: 'Electric Guitar'
+    }
+]
+
+const handlingForms = {
+
+    data() {
+        return {
+            members: window.members,
+            newMember: {}
+        }
+    },
+
+};
+
+Vue.createApp(handlingForms).mount('#app');
